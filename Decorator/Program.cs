4e45fb5f815f3b6486e4d409843f508afc72ece0
Denys <denpendos23@gmail.com>
2024-11-20 14:20:31 +0200

    Add project files.

---
+++ Decorator/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+namespace Decorator.Examples
+{
+    class MainApp
+    {
+        static void Main()
+        {
+
+            ConcreteChristmasTree cе = new ConcreteChristmasTree();
+            GarlandDecorator g1 = new GarlandDecorator();
+            OrnamentDecorator o1 = new OrnamentDecorator();
+            OrnamentDecorator o2 = new OrnamentDecorator();
+            OrnamentDecorator o3 = new OrnamentDecorator();
+
+            // Link decorators
+            g1.SetComponent(cе);
+            o1.SetComponent(g1);
+            o2.SetComponent(g1);
+            o3.SetComponent(g1);
+
+            o1.Operation();
+            o2.Operation();
+            o3.Operation();
+
+            // Wait for user
+            Console.Read();
+        }
+    }
+    // "Component"
+    abstract class ChristmasTree
+    {
+        public abstract void Operation();
+    }
+
+
+    // "ConcreteComponent"
+    class ConcreteChristmasTree : ChristmasTree
+    {
+        public override void Operation()
+        {
+            Console.WriteLine("ConcreteComponent.Operation()");
+        }
+    }
+
+    // "Decorator"
+    abstract class Decorator : ChristmasTree
+    {
+        protected ChristmasTree component;
+
+        public void SetComponent(ChristmasTree component)
+        {
+            this.component = component;
+        }
+        public override void Operation()
+        {
+            if (component != null)
+            {
+                component.Operation();
+            }
+        }
+    }
+
+
+    // "ConcreteDecoratorB" 
+    class GarlandDecorator : Decorator
+    {
+        public override void Operation()
+        {
+            base.Operation();
+            AddGarland();
+        }
+        void AddGarland()
+        { Console.WriteLine("Гірлянда світиться на ялинці"); }
+    }
+
+    // "ConcreteDecoratorB" 
+    class OrnamentDecorator : Decorator
+    {
+        public override void Operation()
+        {
+            base.Operation();
+            AddOrnament();
+        }
+        void AddOrnament()
+        { Console.WriteLine("ялинка прикрашена"); }
+    }
+}
---
+++ Prototype/Program.cs
@@ -0,0 +1,93 @@
+﻿using PrototypeFigure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypeFigure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            IFigure figure = new Rectangle(10, 20);
+            IFigure clonedFigure = figure.Clone();
+            figure.GetInfo();
+            clonedFigure.GetInfo();
+            figure = new Circle(15);
+            clonedFigure = figure.Clone();
+            figure.GetInfo();
+            clonedFigure.GetInfo();
+
+            IFigure figure2 = new Triangle((10, 20), (2, -4), (5, 7));
+            IFigure clonedFigure2 = figure2.Clone();
+            figure2.GetInfo();
+            clonedFigure2.GetInfo();
+
+            Console.Read();
+        }
+    }
+    interface IFigure
+ {
+ IFigure Clone();
+    void GetInfo();
+}
+class Rectangle : IFigure
+{
+    int width;
+    int height;
+    public Rectangle(int w, int h)
+    {
+        width = w;
+        height = h;
+    }
+    public IFigure Clone()
+    {
+        return new Rectangle(this.width, this.height);
+    }
+    public void GetInfo()
+    {
+        Console.WriteLine("Прямокутник довжиною {0} і шириною{1}", height, width);
+    }
+}
+
+    class Triangle : IFigure
+    {
+        (int, int) A;
+        (int, int) B;
+        (int, int) C;
+        public Triangle((int, int) a, (int, int) b, (int, int) c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+        public IFigure Clone()
+        {
+            return new Triangle(this.A, this.B, this.C);
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine("Трикутник з вершинами {0}, {1}, {2}", A, B, C);
+        }
+    }
+
+    class Circle : IFigure
+{
+    int radius;
+    public Circle(int r)
+    {
+        radius = r;
+    }
+    public IFigure Clone()
+    {
+        return new Circle(this.radius);
+    }
+    public void GetInfo()
+    {
+        Console.WriteLine("Круг радіусом {0}", radius);
+    }
+}
+}
